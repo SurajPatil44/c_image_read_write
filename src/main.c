d86f5f3c9444b16f2bac4be910acbb4fcd830ebf
SurajPatil44 <suraj.patil716@gmail.com>
2023-08-27 16:42:34 +0530

    new

--- src/main.c
+++ src/main.c
@@ -83,6 +83,19 @@ ArgErr read_args(int argc,char* argv[],SV *args,SV *ext_found){
    return no_error;
 }
 
+int process_and_save(my_image *src) {
+
+    my_image *dst = new_image();
+    int res = makebw(src,dst);
+    //(void) res;
+    if (res != 0) {
+        printf("report failure \n");
+    }
+    free_image(src);
+    free_image(dst);
+    return 0;
+}
+
 
 
 int main(int argc,char* argv[]) {
@@ -93,30 +106,26 @@ int main(int argc,char* argv[]) {
     switch(res){
         case(no_error): 
             {
-                //
-                //my_image rgb_image; this introduces memory leaks
-                //
-                //
-                //
                 my_image *rgb_image = new_image();
                 char *path = argv[1];
                 if(load_image_from_path(path,rgb_image) != 0) {
-                    res = EXIT_FAILURE;
-                }
-                (void) process_image(rgb_image); 
-                free_image(rgb_image);
+                    result = EXIT_FAILURE;
+                } else {
+                    int nres = process_and_save(rgb_image); 
+                    (void) nres;
+                }//free_image(rgb_image);
                 break;
             }
         case(first_arg_missing) :
             printf("filename is missing\n");
-            res = EXIT_FAILURE;
+            result = EXIT_FAILURE;
             break;
         case(format_not_supported) :
             printf("format "SV_Fmt" not supported\n",SV_arg(ext));
-            res = EXIT_FAILURE;
+            result = EXIT_FAILURE;
             break;
        default:
-            res = EXIT_FAILURE;
+            result = EXIT_FAILURE;
             assert(false && "unreachable");
     }
     return result;
